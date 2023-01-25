edd3ca4a68f029d61fb63f67b84b647c0438cf21
Alfonso <juan.alfonsogs@ciencias.unam.mx>
2023-01-25 00:20:07 -0600

    nose

--- solucion.rkt
+++ solucion.rkt
@@ -3,12 +3,13 @@ SOLUCION PRACTICA 1
 Compiladores 2023-2
 Autor: Juan Alfonso Garduño Solis
 |#
+
 #lang nanopass
 (provide (all-defined-out))
 
 #|-------Ejercicio 1-------|#
 (define (my-map f l)
-  (error "Aqui va tu respuesta"))
+  (error "Sin solucion"))
 
 (define (my-filter p l)
-  (error "Aqui va tu respuesta"))
+  (error "Sin solucion"))
