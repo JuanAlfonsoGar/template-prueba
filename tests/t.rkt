39e6c404617524eb1802b1566168d157524c1038
Alfonso <juan.alfonsogs@ciencias.unam.mx>
2023-01-25 00:29:21 -0600

    noase

--- tests/t.rkt
+++ tests/t.rkt
@@ -15,10 +15,10 @@
 
 (run-tests (test-suite "Ejercicio 1"
     #:before (lambda () (display "my-filter: \n" ))
-    (test-case "my-map"
-        (check-equal? (my-map (lambda (n) (+ n 1)) '(1 2 3 4 5)) '(2 3 4 5 6))
-        (check-equal? (my-map (lambda (n) (- n 1)) '(1 2 3 4 5)) '(0 1 2 3 4))
-        (check-equal? (my-map (lambda (n) (+ n 1)) '()) '())
+    (test-case "my-filter"
+        (check-equal? (my-filter (lambda (n) (> n 1)) '(1 2 3 4 5)) '(2 3 4 5 6))
+        (check-equal? (my-filter (lambda (n) (>= n 1)) '(1 2 3 4 5)) '(1 2 3 4 5))
+        (check-equal? (my-filter (+ 1 2) '()) '())
     'normal)))
 
 
